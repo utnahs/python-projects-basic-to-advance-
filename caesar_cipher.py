43cace46da063fb509c2209a818a3e8bd1def05f
Shantu Sharma <s21111998s@gmail.com>
2026-01-28 11:44:09 +0530

    Add files via upload

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -4,9 +4,6 @@ alphabets = [
     'u','v','w','x','y','z'
 ]
 
-user_choice = input("Type 'encode' to encrypt or 'decode' to decrypt:\n").lower()
-user_message = input("Type your message to:\n").lower()
-shift_value = int(input("Type the shift value:\n"))
 
 
 def encrypt(user_message,shift_value):
@@ -32,11 +29,20 @@ def decrypt(user_message,shift_value):
     print(new_message)
 
 
+continue_=True 
+while continue_:
+    user_choice = input("Type 'encode' to encrypt or 'decode' to decrypt:\n").lower()
+    user_message = input("Type your message to:\n").lower()
+    shift_value = int(input("Type the shift value:\n"))
 
+    if user_choice == 'encode':
+        encrypt(user_message,shift_value)
+    elif user_choice == 'decode':
+        decrypt(user_message,shift_value)
+    else:
+        print("Invalid Input")
 
-if user_choice == 'encode':
-    encrypt(user_message,shift_value)
-elif user_choice == 'decode':
-    decrypt(user_message,shift_value)
-else:
-    print("Invalid Input")
+    a=input("Do you want to go again? Type y for yes and n for no:\n").lower()
+    if a!='y':
+        continue_=False
+
